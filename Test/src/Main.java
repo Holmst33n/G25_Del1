040199e9ea841f5dee7f554ee193a191b0c74262
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2022-09-30 12:58:12 +0200

    Created test enviroment

---
+++ Test/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Main {
+    // Det er i denne selve spillet udvikles
+
+    public static void main(String[] args) {
+
+        // player 1 og player 2 er vores spillere og dicesum er summen af begge terninger
+        // die 1 og die 2 er vores to terninger og Die er vores metode
+
+        int player1, player2, diceSum;
+        Die die1, die2;
+
+        // Her bruger vi igen Die som metode til at lave vores terninger
+        die1 = new Die();
+        die2 = new Die();
+
+        // Player 1 og 2 score er 0 til at starte med
+        player1 = 0;
+        player2 = 0;
+
+        // Så længe at begge spilleres score er under 40, så kører while loppet videre.
+        while (player1 < 40 && player2 < 40) {
+
+            System.out.println("player1 press Enter");
+            Scanner input = new Scanner(System.in);
+
+            String test = input.nextLine();
+
+            // begge terninger kastes
+            die1.roll();
+            die2.roll();
+
+            //Dicesummen er valuen af begge kastede terninger
+            diceSum = die1.getFaceValue() + die2.getFaceValue();
+
+            // System udskriver hvad spiller 1 slog med terningerne
+            // Efter så kommer den til at gemme terningernes værdi til spillerens score
+            // Systemet udskriver spillerens totale score
+            System.out.println("Player one rolled: " + die1.toString() + " and " + die2.toString());
+            player1 = player1 + diceSum;
+            System.out.println("Player ones total score is: " + player1);
+            System.out.println();
+
+            // Hvis spilleren slår over 40, så vinder spiller 1
+            if(player1 > 40) {
+                System.out.println("player one won");
+                break;
+            }
+
+            // Nu er spiller 1's tur færdig og derfor kastes der et nyt kast
+
+            System.out.println("player2 press Enter");
+            test = input.nextLine();
+
+            die1.roll();
+            die2.roll();
+
+            // Dicesummen er valuen af begge kastede terninger
+            diceSum = die1.getFaceValue() + die2.getFaceValue();
+
+
+            // System udskriver hvad spiller 1 slog med terningerne
+            // Efter så kommer den til at gemme terningernes værdi til spillerens score
+            // Systemet udskriver spillerens totale score
+            System.out.println("Player two rolled: " + die1.toString() + " and " + die2.toString());
+            player2 = player2 + diceSum;
+            System.out.println("Player two's total score is: " + player2);
+            System.out.println();
+
+            // Hvis spiller 1 ikke får 40 først, så vinder spiller 2, ved at slå 40.
+            if(player2 > 40){
+                System.out.println("Player two won");
+                break;
+
+            }
+        }
+
+        System.out.print("Thank you for playing!");
+
+    }
+}
